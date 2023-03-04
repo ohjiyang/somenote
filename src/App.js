94743d133d939686d1bedb97dc1d04cc3f95aff2
ohjiyang <ohjiyung@icloud.com>
2023-03-04 20:49:50 +0600

    feat(App): created header/page sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+    return (
+        <div className="App">
+            <section className="header">
+                <div className="header__container __container">
+                    <div className="header__body">header</div>
+                </div>
+            </section>
+            <section className="page">
+                <div className="page__container __container">
+                    <div className="page__body">
+                        page
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
